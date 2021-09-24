44cd28775a178b1f0e904acad7b5981c75ff0404
Syed Zuberia Shabnam <zuberia01@gmail.com>
2021-09-25 02:51:15 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React from "react"
-import Photos  from "./TodoItem"
+import TodoItem  from "./TodoItem.js"
+import "./index.css"
 
 export default class App extends React.Component{
     constructor(){
         super()
         this.state={
-            character:[]
+            character:[],
+            isLoading:false
 
         }
+        this.handleChange = this.handleChange.bind(this)
     }
+
+    //fetching data from API with CDM
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/photos')
+        this.setState({isLoading:true})
+        fetch('https://jsonplaceholder.typicode.com/todos')
         .then(response => response.json())
         .then(data => {
             this.setState({
-                character :data
+                character :data,
+                isLoading:false
             })
     })
     }
+
+    //handleChange Method
+    handleChange(id){
+        this.setState(prevState => {
+            //map Method
+            const updatedTodos = prevState.character.map(item => {
+                //if/else
+                if(item.id === id){
+                    return{
+                        ...item,
+                        completed :!item.completed
+                    }
+                }
+                return item
+            })
+            return{
+                character:updatedTodos
+            }
+        })
+    }
+
    render(){
-       return(
-           <div>
-               {this.state.character.map(item => <TodoItem key={item.id} todo={item}/>)}
-           </div>
-       )
+       if(this.state.isLoading){
+           return(
+               <div className="loading">
+                   <p>WELCOME</p>
+                   <div className="loader"></div>
+               </div>
+           )
+       }
+       else{
+           return(
+               <div className="todo-lists">
+                   <h1>TodoApp-React.js</h1>
+                   {this.state.character.map(item => <TodoItem key={item.id} todo={item} handleChange={this.handleChange}/>)}
+               </div>
+           )
+       }
    }
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,18 @@
 import React from "react"
 
 function TodoItem(props){
+    const styles = {
+        textDecoration:"line-through",
+        color:"#aaa",
+        fontStyle:"italic"
+    }
     return(
-        <div>
-            <p>{props.todo.title}</p>
-            <input type="checkbox" checked={props.todo.completed}/>
+        <div className="todo-list">
+            <input type="checkbox" checked={props.todo.completed}
+            onChange={() => props.handleChange(props.todo.id)
+            }
+            />
+            <p style={props.todo.completed ? styles : null}>{props.todo.title}</p>
         </div>
     )
 }
